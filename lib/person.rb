0fc842078086a0a65488c4e7d7b6463f9a8229bb
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-08-08 13:30:02 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,77 @@
 # your code goes here
+class Person
+attr_reader :name, :friend_name
+attr_accessor :bank_account, :happiness, :hygiene
+
+
+  def initialize(name)
+    @name = name 
+    @bank_account = 25 
+    @happiness = 8
+    @hygiene = 8
+    @friend_name = friend_name
+end
+
+
+
+def happiness=(num)
+  @happiness = num
+  if @happiness > 10 
+    @happiness = 10 
+  elsif @happiness < 0
+    @happiness = 0 
+  else 
+   @happiness
+  end
+end 
+
+def hygiene=(num)
+  @hygiene = num
+  if @hygiene > 10 
+    @hygiene = 10 
+  elsif @hygiene < 0
+    @hygiene = 0 
+  else 
+   @hygiene
+  end
+end 
+
+
+def clean?
+ if @hygiene > 7 
+   return true
+ else
+   return false
+  end 
+end
+
+def happy?
+ if @happiness > 7 
+   return true
+ else
+   return false
+  end 
+end
+
+def get_paid(salary_amount)
+  @bank_account += salary_amount
+  return "all about the benjamins"
+end
+
+def take_bath
+  self.hygiene += 4
+  return "♪ Rub-a-dub just relaxing in the tub ♫"
+end
+
+
+def work_out
+  self.hygiene -= 3
+  self.happiness += 2
+  return "♪ another one bites the dust ♫"
+end
+
+def call_friend(name)
+  
+end 
+
+
